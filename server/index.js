eb29eda2041facd93031800ac5008a699935c95b
meenajii2004 <harshvardhanmeena006@gmail.com>
2024-11-13 14:03:04 +0530

    connected to vercel

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin:["https://paypal-integration-frontend.vercel.app/"],
+        origin:["https://paypal-integration-frontend.vercel.app"],
         methods:["POST","GET"],
         credentials:true
     }
